224535aac8ff455cd99f485c63023dd0cfb5ed1d
hsycamp <hsycamp@gmail.com>
2021-01-08 16:54:12 +0900

    Feat : 회원 조회 서비스 로직 추가

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -29,4 +29,15 @@ public class MemberService {
                 });
     }
 
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
 }
